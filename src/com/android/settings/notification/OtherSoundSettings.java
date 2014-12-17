4c6d7d7b6cb376a6b5cb19a1fc819fb69767b72b
Michael Bestas <mikeioannina@gmail.com>
2014-12-18 00:19:27 +0200

    Settings: Add volume tones back [2/2]
    This patch allows the users to choose if they want the audible tone when adjusting the volume
    
    Change-Id: I6d43cba83bf4cf10f2d50502e7cee5408a73c520
    
    Conflicts:
    	src/com/android/settings/notification/OtherSoundSettings.java

--- src/com/android/settings/notification/OtherSoundSettings.java
+++ src/com/android/settings/notification/OtherSoundSettings.java
@@ -21,6 +21,8 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.database.ContentObserver;
 import android.media.AudioManager;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -67,6 +69,7 @@ public class OtherSoundSettings extends SettingsPreferenceFragment implements In
     private static final String KEY_CHARGING_SOUNDS = "charging_sounds";
     private static final String KEY_DOCKING_SOUNDS = "docking_sounds";
     private static final String KEY_SCREENSHOT_SOUND = "screenshot_sound";
+    private static final String KEY_VOLUME_ADJUST_SOUNDS = "volume_adjust_sounds";
     private static final String KEY_TOUCH_SOUNDS = "touch_sounds";
     private static final String KEY_VIBRATE_ON_TOUCH = "vibrate_on_touch";
     private static final String KEY_DOCK_AUDIO_MEDIA = "dock_audio_media";
@@ -100,6 +103,11 @@ public class OtherSoundSettings extends SettingsPreferenceFragment implements In
 
     private static final SettingPref PREF_SCREENSHOT_SOUND = new SettingPref(
             TYPE_SYSTEM, KEY_SCREENSHOT_SOUND, System.SCREENSHOT_SOUND,
+            DEFAULT_ON) {
+    };
+
+    private static final SettingPref PREF_VOLUME_ADJUST_SOUNDS = new SettingPref(
+            TYPE_SYSTEM, KEY_VOLUME_ADJUST_SOUNDS, System.VOLUME_ADJUST_SOUNDS_ENABLED,
             DEFAULT_ON) {
     };
 
@@ -182,6 +190,7 @@ public class OtherSoundSettings extends SettingsPreferenceFragment implements In
         PREF_CHARGING_SOUNDS,
         PREF_DOCKING_SOUNDS,
         PREF_SCREENSHOT_SOUND,
+        PREF_VOLUME_ADJUST_SOUNDS,
         PREF_TOUCH_SOUNDS,
         PREF_VIBRATE_ON_TOUCH,
         PREF_DOCK_AUDIO_MEDIA,
