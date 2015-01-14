099ae6a9c8b60dbd5de22050900eaa7c1943a12e
Paul Beeler <pbeeler80@gmail.com>
2015-01-13 23:44:22 -0700

    UBERTC: gcc and gcc flags info 2/2
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    Signed-off-by: martinusbe <martinusbe@gmail.com>
    
    Conflicts:
    	src/com/android/settings/DeviceInfoSettings.java
    
    Change-Id: Iec19bc5b04a620dacb3e05ee8b68b5a41dbeb637
    
    Conflicts:
    	res/xml/device_info_settings.xml
    	src/com/android/settings/DeviceInfoSettings.java
    
    Conflicts:
    	src/com/android/settings/DeviceInfoSettings.java

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -84,6 +84,9 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String PROPERTY_EQUIPMENT_ID = "ro.ril.fccid";
     private static final String KEY_DEVICE_FEEDBACK = "device_feedback";
     private static final String KEY_SAFETY_LEGAL = "safetylegal";
+    private static final String KEY_UBER_AND = "uber_android";
+    private static final String KEY_UBER_KERNEL = "uber_kernel";
+    private static final String KEY_UBER_FLAGS = "uber_flags";
 
     static final int TAPS_TO_BE_A_DEVELOPER = 7;
     private static boolean mHideVersionName = false;
@@ -131,6 +134,13 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         setStringSummary(KEY_DEVICE_MODEL, Build.MODEL);
         setStringSummary(KEY_BUILD_NUMBER, Build.DISPLAY);
         findPreference(KEY_BUILD_NUMBER).setEnabled(true);
+        setStringSummary(KEY_KERNEL_VERSION, getFormattedKernelVersion());
+        findPreference(KEY_KERNEL_VERSION).setEnabled(true);
+        findPreference(KEY_UBER_KERNEL).setEnabled(true);
+        findPreference(KEY_UBER_FLAGS).setEnabled(true);
+        setValueSummary(KEY_UBER_AND, "ro.uber.android");
+        setValueSummary(KEY_UBER_KERNEL, "ro.uber.kernel");
+        setValueSummary(KEY_UBER_FLAGS, "ro.uber.flags");
         setValueSummary(KEY_QGP_VERSION, PROPERTY_QGP_VERSION);
         findPreference(KEY_KERNEL_VERSION).setSummary(getFormattedKernelVersion());
 
