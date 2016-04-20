c82a1bda09e45f04297ae257f8b985e4abe1dafa
Willi Ye <williye97@gmail.com>
2016-04-20 17:28:49 +0200

    batterystyles: Remove horizontal icon
    Change-Id: Iae663e3d940fc2d00611a225f27c3f42cbe077d9
    
    Conflicts:
    	src/com/android/settings/fuelgauge/PowerUsageSummary.java

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -79,14 +79,13 @@ public class PowerUsageSummary extends PowerUsageBase {
     private static final int MENU_BATTERY_BAR = Menu.FIRST + 5;
     private static final int MENU_BATTERY_STYLE             = Menu.FIRST + 6;
     private static final int SUBMENU_BATTERY_BAR            = Menu.FIRST + 7;
-    private static final int SUBMENU_BATTERY_BAR_LANDSCAPE  = Menu.FIRST + 8;
-    private static final int SUBMENU_BATTERY_CIRCLE         = Menu.FIRST + 9;
-    private static final int SUBMENU_BATTERY_TEXT           = Menu.FIRST + 10;
-    private static final int SUBMENU_BATTERY_HIDDEN         = Menu.FIRST + 11;
-    private static final int MENU_BATTERY_PERCENT           = Menu.FIRST + 12;
-    private static final int SUBMENU_BATTERY_PERCENT_HIDDEN = Menu.FIRST + 13;
-    private static final int SUBMENU_BATTERY_PERCENT_INSIDE = Menu.FIRST + 14;
-    private static final int SUBMENU_BATTERY_PERCENT_NEXT   = Menu.FIRST + 15;
+    private static final int SUBMENU_BATTERY_CIRCLE         = Menu.FIRST + 8;
+    private static final int SUBMENU_BATTERY_TEXT           = Menu.FIRST + 9;
+    private static final int SUBMENU_BATTERY_HIDDEN         = Menu.FIRST + 10;
+    private static final int MENU_BATTERY_PERCENT           = Menu.FIRST + 11;
+    private static final int SUBMENU_BATTERY_PERCENT_HIDDEN = Menu.FIRST + 12;
+    private static final int SUBMENU_BATTERY_PERCENT_INSIDE = Menu.FIRST + 13;
+    private static final int SUBMENU_BATTERY_PERCENT_NEXT   = Menu.FIRST + 14;
 
     private BatteryHistoryPreference mHistPref;
     private PreferenceGroup mAppListGroup;
@@ -172,10 +171,8 @@ public class PowerUsageSummary extends PowerUsageBase {
 
         SubMenu batteryStyle = menu.addSubMenu(1, MENU_BATTERY_STYLE, 1, R.string.battery_style_title);
 
-        batteryStyle.add(1, SUBMENU_BATTERY_BAR, 1, R.string.battery_style_icon_portrait)
+        batteryStyle.add(1, SUBMENU_BATTERY_BAR, 1, R.string.battery_style_icon)
                     .setChecked(selectedIcon == 0);
-        batteryStyle.add(1, SUBMENU_BATTERY_BAR_LANDSCAPE, 2, R.string.battery_style_icon_landscape)
-                    .setChecked(selectedIcon == 5);
         batteryStyle.add(1, SUBMENU_BATTERY_CIRCLE, 3, R.string.battery_style_circle)
                     .setChecked(selectedIcon == 2);
         batteryStyle.add(1, SUBMENU_BATTERY_TEXT, 4, R.string.battery_style_text)
@@ -244,11 +241,6 @@ public class PowerUsageSummary extends PowerUsageBase {
                 item.setChecked(true);
                 Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.STATUS_BAR_BATTERY_STYLE, 0);
-                return true;
-            case SUBMENU_BATTERY_BAR_LANDSCAPE:
-                item.setChecked(true);
-                Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.STATUS_BAR_BATTERY_STYLE, 5);
                 return true;
             case SUBMENU_BATTERY_CIRCLE:
                 item.setChecked(true);
