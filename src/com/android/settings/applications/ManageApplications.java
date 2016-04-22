4bcc87e33c5e7ed85f11b6326469290b9d2f7a8e
thecrazyskull <anaskarbila@aospa.co>
2016-04-22 15:20:55 -0400

    [2/2]NotificationSettings: add floating headsup blacklist
    Change-Id: I0e1b166b04138a32d8c0055ec05715d6f3896e1a
    
    Conflicts:
    	res/values/pa_strings.xml

--- src/com/android/settings/applications/ManageApplications.java
+++ src/com/android/settings/applications/ManageApplications.java
@@ -141,6 +141,7 @@ public class ManageApplications extends InstrumentedFragment
     public static final int FILTER_APPS_USAGE_ACCESS            = 12;
     public static final int FILTER_APPS_WITH_OVERLAY            = 13;
     public static final int FILTER_APPS_WRITE_SETTINGS          = 14;
+    public static final int FILTER_APPS_NO_FLOATING             = 15;
 
     // This is the string labels for the filter modes above, the order must be kept in sync.
     public static final int[] FILTER_LABELS = new int[] {
@@ -152,6 +153,7 @@ public class ManageApplications extends InstrumentedFragment
         R.string.filter_notif_blocked_apps,   // Blocked Notifications
         R.string.filter_notif_priority_apps,  // Priority Notifications
         R.string.filter_notif_no_peeking,     // No peeking Notifications
+        R.string.filter_notif_no_floating,    // No peeking floating
         R.string.filter_notif_sensitive_apps, // Sensitive Notifications
         R.string.filter_personal_apps, // Personal
         R.string.filter_work_apps,     // Work
@@ -173,6 +175,7 @@ public class ManageApplications extends InstrumentedFragment
         AppStateNotificationBridge.FILTER_APP_NOTIFICATION_BLOCKED,   // Blocked Notifications
         AppStateNotificationBridge.FILTER_APP_NOTIFICATION_PRIORITY,  // Priority Notifications
         AppStateNotificationBridge.FILTER_APP_NOTIFICATION_NO_PEEK,   // No peeking Notifications
+        AppStateNotificationBridge.FILTER_APP_NOTIFICATION_NO_FLOATING, // No floating peek notifications
         AppStateNotificationBridge.FILTER_APP_NOTIFICATION_SENSITIVE, // Sensitive Notifications
         ApplicationsState.FILTER_PERSONAL,    // Personal
         ApplicationsState.FILTER_WORK,        // Work
@@ -392,6 +395,7 @@ public class ManageApplications extends InstrumentedFragment
             mFilterAdapter.enableFilter(FILTER_APPS_PRIORITY);
             mFilterAdapter.enableFilter(FILTER_APPS_SENSITIVE);
             mFilterAdapter.enableFilter(FILTER_APPS_NO_PEEKING);
+            mFilterAdapter.enableFilter(FILTER_APPS_NO_FLOATING);
         }
         if (mListType == LIST_TYPE_HIGH_POWER) {
             mFilterAdapter.enableFilter(FILTER_APPS_POWER_WHITELIST_ALL);
