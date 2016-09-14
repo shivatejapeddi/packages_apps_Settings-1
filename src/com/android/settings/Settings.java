00a70f6dbdb6f5abfc962d848ed01e8631fa422e
iskandar1023 <iiiiskandar14@gmail.com>
2016-09-15 00:46:57 +0700

    Add Substratum dashboard tile
    Change-Id: Iefe59e5cce93fa1c0f55b89a9b0b3682bda4d917
    PS2: Change package detection logic
    
    Conflicts:
    	AndroidManifest.xml
    	res/values/custom_strings.xml
    	src/com/android/settings/SettingsActivity.java
    
    Conflicts:
    	AndroidManifest.xml
    	src/com/android/settings/SettingsActivity.java

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -161,11 +161,12 @@ public class Settings extends SettingsActivity {
     public static class WifiAPITestActivity extends SettingsActivity { /* empty */ }
     public static class WifiInfoActivity extends SettingsActivity { /* empty */ }
     public static class SMQQtiFeedbackActivity extends SettingsActivity { /* empty */ }
+    public static class SubstratumLaunchActivity extends SettingsActivity { /* empty */ }
 
     // Categories.
     public static class WirelessSettings extends SettingsActivity { /* empty */ }
     public static class DeviceSettings extends SettingsActivity { /* empty */ }
     public static class PersonalSettings extends SettingsActivity { /* empty */ }
     public static class SystemSettings extends SettingsActivity { /* empty */ }
-    public static class CustomSquashActivity extends SettingsActivity { /* empty */ } 
+    public static class CustomSquashActivity extends SettingsActivity { /* empty */ }
 }
