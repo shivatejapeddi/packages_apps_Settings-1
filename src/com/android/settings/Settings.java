bf05e15c2c1f1a47f9523c76f6d01287767f55de
Alex Cruz <mazdarider23@gmail.com>
2016-09-26 10:07:29 +0200

    [QUICK SETTINGS] Expanded Desktop tile longpress action
    - Fix sync tile longpress
    
    Change-Id: Ib9b7a875165828e693d2de0479886f1d91e98df1

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -167,5 +167,6 @@ public class Settings extends SettingsActivity {
     public static class DeviceSettings extends SettingsActivity { /* empty */ }
     public static class PersonalSettings extends SettingsActivity { /* empty */ }
     public static class SystemSettings extends SettingsActivity { /* empty */ }
-    public static class CustomSquashActivity extends SettingsActivity { /* empty */ } 
+    public static class CustomSquashActivity extends SettingsActivity { /* empty */ }
+    public static class ExpandedDesktopSettingsActivity extends SettingsActivity { /* empty */ }
 }
