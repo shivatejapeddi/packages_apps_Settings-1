AOSPA/android_packages_apps_Settings

DisplaySettings: add support sRGB toggle
Change-Id: Ifdce3cedeff680beedcb7efe30fc9b573a86032a

src/com/android/settings/DisplaySettings.java
@@ -71,6 +71,7 @@ import java.util.List;
 import static android.provider.Settings.Secure.CAMERA_GESTURE_DISABLED;
 import static android.provider.Settings.Secure.DOUBLE_TAP_TO_WAKE;
 import static android.provider.Settings.Secure.DOZE_ENABLED;
+import static android.provider.Settings.Secure.SRGB_ENABLED;
 import static android.provider.Settings.Secure.WAKE_GESTURE_ENABLED;
 import static android.provider.Settings.System.SCREEN_BRIGHTNESS_MODE;
 import static android.provider.Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC;
@@ -98,6 +99,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private static final String KEY_LIFT_TO_WAKE = "lift_to_wake";
     private static final String KEY_DOZE = "doze";
     private static final String KEY_TAP_TO_WAKE = "tap_to_wake";
+    private static final String KEY_SRGB = "srgb";
     private static final String KEY_AUTO_BRIGHTNESS = "auto_brightness";
     private static final String KEY_AUTO_ROTATE = "auto_rotate";
     private static final String KEY_NIGHT_DISPLAY = "night_display";
@@ -132,6 +134,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private SwitchPreference mLiftToWakePreference;
     private SwitchPreference mDozePreference;
     private SwitchPreference mTapToWakePreference;
+    private SwitchPreference mSrgbPreference;
     private SwitchPreference mAutoBrightnessPreference;
     private SwitchPreference mCameraGesturePreference;
     private SwitchPreference mCameraDoubleTapPowerGesturePreference;
@@ -218,6 +221,13 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
             mTapToWakePreference.setOnPreferenceChangeListener(this);
         } else {
             removePreference(KEY_TAP_TO_WAKE);
+        }
+
+        if (isSrgbAvailable(activity)) {
+            mSrgbPreference = (SwitchPreference) findPreference(KEY_SRGB);
+            mSrgbPreference.setOnPreferenceChangeListener(this);
+        } else {
+            removePreference(KEY_SRGB);
         }
 
         if (isCameraGestureAvailable(getResources())) {
@@ -344,6 +354,10 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
 
     private static boolean isTapToWakeAvailable(Resources res) {
         return res.getBoolean(com.android.internal.R.bool.config_supportDoubleTapWake);
+    }
+
+    private static boolean isSrgbAvailable(Context context) {
+        return !TextUtils.isEmpty(context.getString(com.android.internal.R.string.srgb_path));
     }
 
     private static boolean isAutomaticBrightnessAvailable(Resources res) {
@@ -484,6 +498,10 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
             mDozePreference.setChecked(value != 0);
         }
 
+        if (mSrgbPreference != null) {
+            int value = Settings.Secure.getInt(getContentResolver(), SRGB_ENABLED, 0);
+            mSrgbPreference.setChecked(value != 0);
+        }
         // Update camera gesture #1 if it is available.
         if (mCameraGesturePreference != null) {
             int value = Settings.Secure.getInt(getContentResolver(), CAMERA_GESTURE_DISABLED, 0);
@@ -554,6 +572,10 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         if (preference == mTapToWakePreference) {
             boolean value = (Boolean) objValue;
             Settings.Secure.putInt(getContentResolver(), DOUBLE_TAP_TO_WAKE, value ? 1 : 0);
+        }
+        if (preference == mSrgbPreference) {
+            boolean value = (Boolean) objValue;
+            Settings.Secure.putInt(getContentResolver(), SRGB_ENABLED, value ? 1 : 0);
         }
         if (preference == mCameraGesturePreference) {
             boolean value = (Boolean) objValue;
@@ -690,6 +712,9 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
                     }
                     if (!isDozeAvailable(context)) {
                         result.add(KEY_DOZE);
+                    }
+                    if (!isSrgbAvailable(context)) {
+                        result.add(KEY_SRGB);
                     }
                     if (!RotationPolicy.isRotationLockToggleVisible(context)) {
                         result.add(KEY_AUTO_ROTATE);

shivatejapeddi/packages_apps_Settings-1

src/com/android/settings/DisplaySettings.java
@@ -77,6 +77,7 @@ import java.util.Set;
 import static android.provider.Settings.Secure.CAMERA_GESTURE_DISABLED;
 import static android.provider.Settings.Secure.DOUBLE_TAP_TO_WAKE;
 import static android.provider.Settings.Secure.DOZE_ENABLED;
+import static android.provider.Settings.Secure.SRGB_ENABLED;
 import static android.provider.Settings.Secure.WAKE_GESTURE_ENABLED;
 import static android.provider.Settings.System.SCREEN_BRIGHTNESS_MODE;
 import static android.provider.Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC;
@@ -104,6 +105,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private static final String KEY_LIFT_TO_WAKE = "lift_to_wake";
     private static final String KEY_DOZE = "doze";
     private static final String KEY_TAP_TO_WAKE = "tap_to_wake";
+    private static final String KEY_SRGB = "srgb";
     private static final String KEY_AUTO_BRIGHTNESS = "auto_brightness";
     private static final String KEY_NIGHT_DISPLAY = "night_display";
     private static final String KEY_NIGHT_MODE = "night_mode";
@@ -149,6 +151,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private SwitchPreference mLiftToWakePreference;
     private SwitchPreference mDozePreference;
     private SwitchPreference mTapToWakePreference;
+    private SwitchPreference mSrgbPreference;
     private SwitchPreference mAutoBrightnessPreference;
     private SwitchPreference mCameraGesturePreference;
     private SwitchPreference mCameraDoubleTapPowerGesturePreference;
@@ -248,6 +251,13 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
             removePreference(KEY_TAP_TO_WAKE);
         }
 
+        if (isSrgbAvailable(activity)) {
+            mSrgbPreference = (SwitchPreference) findPreference(KEY_SRGB);
+            mSrgbPreference.setOnPreferenceChangeListener(this);
+        } else {
+            removePreference(KEY_SRGB);
+        }
+
         if (isCameraGestureAvailable(getResources())) {
             mCameraGesturePreference = (SwitchPreference) findPreference(KEY_CAMERA_GESTURE);
             mCameraGesturePreference.setOnPreferenceChangeListener(this);
@@ -344,6 +354,10 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
 
     private static boolean isTapToWakeAvailable(Resources res) {
         return res.getBoolean(com.android.internal.R.bool.config_supportDoubleTapWake);
+    }
+
+    private static boolean isSrgbAvailable(Context context) {
+        return !TextUtils.isEmpty(context.getString(com.android.internal.R.string.config_srgb_path));
     }
 
     private static boolean isAutomaticBrightnessAvailable(Resources res) {
@@ -577,6 +591,10 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
             mDozePreference.setChecked(value != 0);
         }
 
+        if (mSrgbPreference != null) {
+            int value = Settings.Secure.getInt(getContentResolver(), SRGB_ENABLED, 0);
+            mSrgbPreference.setChecked(value != 0);
+        }
         // Update camera gesture #1 if it is available.
         if (mCameraGesturePreference != null) {
             int value = Settings.Secure.getInt(getContentResolver(), CAMERA_GESTURE_DISABLED, 0);
@@ -647,6 +665,10 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         if (preference == mTapToWakePreference) {
             boolean value = (Boolean) objValue;
             Settings.Secure.putInt(getContentResolver(), DOUBLE_TAP_TO_WAKE, value ? 1 : 0);
+        }
+        if (preference == mSrgbPreference) {
+            boolean value = (Boolean) objValue;
+            Settings.Secure.putInt(getContentResolver(), SRGB_ENABLED, value ? 1 : 0);
         }
         if (preference == mCameraGesturePreference) {
             boolean value = (Boolean) objValue;
@@ -799,6 +821,9 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
                     }
                     if (!isDozeAvailable(context)) {
                         result.add(KEY_DOZE);
+                    }
+                    if (!isSrgbAvailable(context)) {
+                        result.add(KEY_SRGB);
                     }
                     if (!RotationPolicy.isRotationLockToggleVisible(context)) {
                         result.add(KEY_ACCELEROMETER);