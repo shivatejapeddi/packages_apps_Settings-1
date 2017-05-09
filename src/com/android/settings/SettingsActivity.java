iskandar1023/packages_apps_Settings

Add Substratum dashboard tile
PS2: Change package detection logic

src/com/android/settings/SettingsActivity.java
@@ -116,6 +116,7 @@ import com.android.settings.sim.SimSettings;
 import com.android.settings.tts.TextToSpeechSettings;
 import com.android.settings.users.UserSettings;
 import com.android.settings.velvet.CustomizationSettings;
+import com.android.settings.velvet.SubstratumLaunch;
 import com.android.settings.vpn2.VpnSettings;
 import com.android.settings.wfd.WifiDisplaySettings;
 import com.android.settings.widget.SwitchBar;
@@ -346,7 +347,8 @@ public class SettingsActivity extends SettingsDrawerActivity
             TestingSettings.class.getName(),
             WifiAPITest.class.getName(),
             WifiInfo.class.getName(),
-            CustomizationSettings.class.getName()
+            CustomizationSettings.class.getName(),
+            SubstratumLaunch.class.getName()
     };
 
 
@@ -1080,6 +1082,15 @@ public class SettingsActivity extends SettingsDrawerActivity
         setTileEnabled(new ComponentName(packageName,
                 Settings.PrintSettingsActivity.class.getName()),
                 pm.hasSystemFeature(PackageManager.FEATURE_PRINTING), isAdmin, pm);
+
+        boolean substratumSupported = false;
+        try {
+            substratumSupported = (pm.getPackageInfo("projekt.substratum", 0).versionCode > 0);
+        } catch (PackageManager.NameNotFoundException e) {
+        }
+        setTileEnabled(new ComponentName(packageName,
+                Settings.SubstratumLaunchActivity.class.getName()),
+                substratumSupported, isAdmin, pm);
 
         final boolean showDev = mDevelopmentPreferences.getBoolean(
                     DevelopmentSettings.PREF_SHOW, android.os.Build.TYPE.equals("eng"))

src/com/android/settings/velvet/SubstratumLaunch.java
@@ -1,0 +1,31 @@
+package com.android.settings.velvet;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+public class SubstratumLaunch extends SettingsPreferenceFragment {
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.APPLICATION;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent i = new Intent();
+        PackageManager pm = getActivity().getPackageManager();
+        i = pm.getLaunchIntentForPackage("projekt.substratum");
+        i.addCategory(Intent.CATEGORY_LAUNCHER);
+        startActivity(i);
+        finish();
+    }
+}

shivatejapeddi/packages_apps_Settings-1

Change-Id: Iefe59e5cce93fa1c0f55b89a9b0b3682bda4d917
Conflicts:
	AndroidManifest.xml
	res/values/custom_strings.xml
	src/com/android/settings/SettingsActivity.java

src/com/android/settings/SettingsActivity.java
@@ -78,6 +78,7 @@ import com.android.settings.applications.UsageAccessDetails;
 import com.android.settings.applications.VrListenerSettings;
 import com.android.settings.applications.WriteSettingsDetails;
 import com.android.settings.bluetooth.BluetoothSettings;
+import com.android.settings.citrus.SubstratumLaunch;
 import com.android.settings.dashboard.DashboardContainerFragment;
 import com.android.settings.dashboard.SearchResultsSummary;
 import com.android.settings.datausage.DataUsageSummary;
@@ -142,6 +143,8 @@ import com.android.settingslib.drawer.Tile;
 import com.citrus.settings.CustomSquash;
 
 import java.net.URISyntaxException;
+
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -387,7 +390,8 @@ public class SettingsActivity extends SettingsDrawerActivity
             NightDisplaySettings.class.getName(),
             ManageDomainUrls.class.getName(),
             AutomaticStorageManagerSettings.class.getName(),
-            CustomSquash.class.getName()
+            CustomSquash.class.getName(),
+            SubstratumLaunch.class.getName()
     };
 
 
@@ -1269,6 +1273,15 @@ public class SettingsActivity extends SettingsDrawerActivity
         setTileEnabled(new ComponentName(packageName,
                 Settings.ProfileMgrMainActivity.class.getName()),
                 getResources().getBoolean(R.bool.config_profilemgrmain_enabled), isAdmin, pm);
+
+        boolean substratumSupported = false;
+        try {
+            substratumSupported = (pm.getPackageInfo("projekt.substratum", 0).versionCode > 0);
+        } catch (PackageManager.NameNotFoundException e) {
+        }
+        setTileEnabled(new ComponentName(packageName,
+                Settings.SubstratumLaunchActivity.class.getName()),
+                substratumSupported, isAdmin, pm);
 
         final boolean showDev = mDevelopmentPreferences.getBoolean(
                     DevelopmentSettings.PREF_SHOW, android.os.Build.TYPE.equals("eng") || android.os.Build.TYPE.equals("userdebug"))

src/com/android/settings/citrus/SubstratumLaunch.java
@@ -1,0 +1,31 @@
+package com.android.settings.citrus;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+public class SubstratumLaunch extends SettingsPreferenceFragment {
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.APPLICATION;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent i = new Intent();
+        PackageManager pm = getActivity().getPackageManager();
+        i = pm.getLaunchIntentForPackage("projekt.substratum");
+        i.addCategory(Intent.CATEGORY_LAUNCHER);
+        startActivity(i);
+        finish();
+    }
+}