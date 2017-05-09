Adarsh-MR/platform_packages_apps_Settings-2

Integrate CustomSquash into Settings
Thanks to @camcory for the manifest part, big help there

src/com/android/settings/SettingsActivity.java
@@ -54,6 +54,7 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.SearchView;
+import com.citrus.settings.CustomSquash;
 import com.android.internal.util.ArrayUtils;
 import com.android.settings.Settings.WifiSettingsActivity;
 import com.android.settings.accessibility.AccessibilitySettings;
@@ -371,6 +372,7 @@ public class SettingsActivity extends SettingsDrawerActivity
             TestingSettings.class.getName(),
             WifiAPITest.class.getName(),
             WifiInfo.class.getName(),
+            CustomSquash.class.getName(),
     };
 
 

shivatejapeddi/packages_apps_Settings-1

src/com/android/settings/SettingsActivity.java
@@ -139,6 +139,8 @@ import com.android.settingslib.drawer.DashboardCategory;
 import com.android.settingslib.drawer.SettingsDrawerActivity;
 import com.android.settingslib.drawer.Tile;
 
+import com.citrus.settings.CustomSquash;
+
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
@@ -384,7 +386,8 @@ public class SettingsActivity extends SettingsDrawerActivity
             MasterClear.class.getName(),
             NightDisplaySettings.class.getName(),
             ManageDomainUrls.class.getName(),
-            AutomaticStorageManagerSettings.class.getName()
+            AutomaticStorageManagerSettings.class.getName(),
+            CustomSquash.class.getName()
     };
 
 