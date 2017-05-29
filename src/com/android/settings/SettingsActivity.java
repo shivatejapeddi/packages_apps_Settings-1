kimjisoo/packages_apps_Settings

DUI: Initial N checkin
Change-Id: If8d277d124e2844810482a52bfb951549ee8c999

src/com/android/settings/SettingsActivity.java
@@ -129,6 +129,10 @@ import com.android.settingslib.drawer.SettingsDrawerActivity;
 import com.android.settingslib.drawer.Tile;
 
 import com.jisoo.tweaks.Tweaks;
+import com.dirtyunicorns.dutweaks.fragments.FlingSettings;
+import com.dirtyunicorns.dutweaks.fragments.NavbarSettings;
+import com.dirtyunicorns.dutweaks.fragments.SmartbarSettings;
+import com.dirtyunicorns.dutweaks.fragments.PulseSettings;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -348,6 +352,10 @@ public class SettingsActivity extends SettingsDrawerActivity
             WifiAPITest.class.getName(),
             WifiInfo.class.getName(),
             Tweaks.class.getName(),
+            NavbarSettings.class.getName(),
+            FlingSettings.class.getName(),
+            SmartbarSettings.class.getName(),
+            PulseSettings.class.getName()
     };
 
 

shivatejapeddi/packages_apps_Settings-1

Conflicts:
	src/com/android/settings/SettingsActivity.java
	Android.mk

src/com/android/settings/SettingsActivity.java
@@ -141,6 +141,10 @@ import com.android.settingslib.drawer.Tile;
 
 import com.citrus.settings.CustomSquash;
 import com.citrus.settings.fragments.ExpandedDesktop;
+import com.citrus.settings.fragments.FlingSettings;
+import com.citrus.settings.fragments.NavbarSettings;
+import com.citrus.settings.fragments.SmartbarSettings;
+import com.citrus.settings.fragments.PulseSettings;
 
 import java.net.URISyntaxException;
 import java.util.ArrayList;
@@ -389,7 +393,11 @@ public class SettingsActivity extends SettingsDrawerActivity
             ManageDomainUrls.class.getName(),
             AutomaticStorageManagerSettings.class.getName(),
             CustomSquash.class.getName(),
-            ExpandedDesktop.class.getName()
+            ExpandedDesktop.class.getName(),
+            NavbarSettings.class.getName(),
+            FlingSettings.class.getName(),
+            SmartbarSettings.class.getName(),
+            PulseSettings.class.getName()
     };
 
 