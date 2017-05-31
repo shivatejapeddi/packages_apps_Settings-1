DirtyUnicorns/android_packages_apps_Settings

[QUICK SETTINGS] Expanded Desktop tile longpress action
- Fix sync tile longpress

Change-Id: I9aba0a9bf2b211267d5c2a5a22537cc669b179bb

src/com/android/settings/SettingsActivity.java
@@ -129,6 +129,7 @@ import com.android.settingslib.drawer.SettingsDrawerActivity;
 import com.android.settingslib.drawer.Tile;
 
 import com.dirtyunicorns.dutweaks.DirtyTweaks;
+import com.dirtyunicorns.dutweaks.fragments.ExpandedDesktop;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -350,6 +351,7 @@ public class SettingsActivity extends SettingsDrawerActivity
             WifiAPITest.class.getName(),
             WifiInfo.class.getName(),
             DirtyTweaks.class.getName(),
+            ExpandedDesktop.class.getName(),
     };
 
 

shivatejapeddi/packages_apps_Settings-1

Change-Id: Ib9b7a875165828e693d2de0479886f1d91e98df1

src/com/android/settings/SettingsActivity.java
@@ -140,6 +140,7 @@ import com.android.settingslib.drawer.SettingsDrawerActivity;
 import com.android.settingslib.drawer.Tile;
 
 import com.citrus.settings.CustomSquash;
+import com.citrus.settings.fragments.ExpandedDesktop;
 
 import java.net.URISyntaxException;
 import java.util.ArrayList;
@@ -387,7 +388,8 @@ public class SettingsActivity extends SettingsDrawerActivity
             NightDisplaySettings.class.getName(),
             ManageDomainUrls.class.getName(),
             AutomaticStorageManagerSettings.class.getName(),
-            CustomSquash.class.getName()
+            CustomSquash.class.getName(),
+            ExpandedDesktop.class.getName()
     };
 
 