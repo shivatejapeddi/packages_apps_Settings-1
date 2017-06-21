DirtyUnicorns/android_packages_apps_Settings

Add OmniJaws longpress/tile 'more settings' action
- This is combination of service settings and icon pack because
we want to store the icon pack in a Settings.System store

This is a hidden tile that only accessible via the WeatherTile
- more settings - button

Change-Id: I178ff7eae2e9978b19cda30567b337b07f6a42f7

src/com/android/settings/SettingsActivity.java
@@ -140,6 +140,7 @@ import com.dirtyunicorns.dutweaks.fragments.ExpandedDesktop;
 import com.dirtyunicorns.dutweaks.fragments.FlingSettings;
 import com.dirtyunicorns.dutweaks.fragments.HeadsUpSettings;
 import com.dirtyunicorns.dutweaks.fragments.NavbarSettings;
+import com.dirtyunicorns.dutweaks.fragments.QuickSettings;
 import com.dirtyunicorns.dutweaks.fragments.SmartbarSettings;
 import com.dirtyunicorns.dutweaks.fragments.PulseSettings;
 
@@ -384,7 +385,8 @@ public class SettingsActivity extends SettingsDrawerActivity
             ManageDomainUrls.class.getName(),
             AutomaticStorageManagerSettings.class.getName(),
             HeadsUpSettings.class.getName(),
-            DataSaverSummary.class.getName()
+            DataSaverSummary.class.getName(),
+            QuickSettings.class.getName()
     };
 
 

shivatejapeddi/packages_apps_Settings-1

Signed-off-by: PhenomX1998 <cool.leo.aditya@gmail.com>

src/com/android/settings/SettingsActivity.java
@@ -145,6 +145,7 @@ import com.citrus.settings.fragments.FlingSettings;
 import com.citrus.settings.fragments.NavbarSettings;
 import com.citrus.settings.fragments.SmartbarSettings;
 import com.citrus.settings.fragments.PulseSettings;
+import com.citrus.settings.fragments.QuickSettings;
 
 import java.net.URISyntaxException;
 import java.util.ArrayList;
@@ -397,7 +398,8 @@ public class SettingsActivity extends SettingsDrawerActivity
             NavbarSettings.class.getName(),
             FlingSettings.class.getName(),
             SmartbarSettings.class.getName(),
-            PulseSettings.class.getName()
+            PulseSettings.class.getName(),
+            QuickSettings.class.getName()
     };
 
 