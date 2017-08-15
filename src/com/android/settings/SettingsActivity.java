nitrogen-project/android_packages_apps_Settings

add activity to launch headsup settings fragment

src/com/android/settings/SettingsActivity.java
@@ -138,6 +138,7 @@ import com.nitrogen.settings.NitrogenSettings;
 
 import java.net.URISyntaxException;
 import com.nitrogen.settings.fragments.FlingSettings;
+import com.nitrogen.settings.fragments.HeadsUpSettings;
 import com.nitrogen.settings.fragments.NavbarSettings;
 import com.nitrogen.settings.fragments.SmartbarSettings;
 import com.nitrogen.settings.fragments.PulseSettings;
@@ -378,7 +379,8 @@ public class SettingsActivity extends SettingsDrawerActivity
             ExpandedDesktopPreferenceFragment.class.getName(),
             ManageDomainUrls.class.getName(),
             AutomaticStorageManagerSettings.class.getName(),
-            ButtonSettings.class.getName()
+            ButtonSettings.class.getName(),
+            HeadsUpSettings.class.getName(),
     };
 
 

shivatejapeddi/packages_apps_Settings-1

src/com/android/settings/SettingsActivity.java
@@ -144,6 +144,7 @@ import com.citrus.settings.CustomSquash;
 import com.citrus.settings.fragments.ExpandedDesktop;
 import com.citrus.settings.fragments.FlingSettings;
 import com.citrus.settings.fragments.HardwareKeys;
+import com.citrus.settings.fragments.HeadsUpSettings;
 import com.citrus.settings.fragments.NavbarSettings;
 import com.citrus.settings.fragments.SmartbarSettings;
 import com.citrus.settings.fragments.PulseSettings;
@@ -403,7 +404,8 @@ public class SettingsActivity extends SettingsDrawerActivity
             PulseSettings.class.getName(),
             QuickSettings.class.getName(),
             AmbientSettings.class.getName(),
-            HardwareKeys.class.getName()
+            HardwareKeys.class.getName(),
+            HeadsUpSettings.class.getName()
     };
 
 