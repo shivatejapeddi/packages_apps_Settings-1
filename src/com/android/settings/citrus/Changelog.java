nitrogen-project/android_packages_apps_Settings

Fix Changelog menu back action

src/com/android/settings/nitrogen/Changelog.java
@@ -25,12 +25,20 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
 
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Changelog extends Fragment {
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+public class Changelog extends SettingsPreferenceFragment {
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.NITROGEN_SETTINGS;
+    }
 
     private static final String CHANGELOG_PATH = "/system/etc/Changelog.txt";
 

shivatejapeddi/packages_apps_Settings-1

src/com/android/settings/citrus/Changelog.java
@@ -25,12 +25,20 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
 
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Changelog extends Fragment {
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+public class Changelog extends SettingsPreferenceFragment {
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.CUSTOM_SQUASH;
+    }
 
     private static final String CHANGELOG_PATH = "/system/etc/Changelog.txt";
 