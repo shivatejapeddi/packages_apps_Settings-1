24729378f2c58d4bdca617dddbf5dfcb27219fdd
maxwen <max.weninger@gmail.com>
2017-08-31 00:30:09 +0200

    Setting: Integrate CustomSquash into Settings Homepage
    * Citrus-CAF edits:
      Adapt to Androoid Pie by adding background
    
    Change-Id: Iaa52c877d343f75c7549b16ecfcc71aa8f180904

--- Android.mk
+++ Android.mk
@@ -19,6 +19,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_USE_AAPT2 := true
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_SRC_FILES += $(call all-java-files-under, ../CustomSquash/src)
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-slices-builders \
@@ -31,7 +32,7 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-support-v7-cardview \
     android-support-v7-preference \
     android-support-v7-recyclerview \
-    android-support-v14-preference \
+    android-support-v14-preference
 
 LOCAL_JAVA_LIBRARIES := \
     bouncycastle \
@@ -44,9 +45,23 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     android-arch-lifecycle-extensions \
     guava \
     jsr305 \
-    settings-logtags \
+    settings-logtags
+
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
+    frameworks/support/v7/appcompat/res \
+    frameworks/support/v7/preference/res \
+    frameworks/support/v7/recyclerview/res \
+    frameworks/support/v14/preference/res \
+    packages/apps/CustomSquash/res
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+LOCAL_AAPT_FLAGS := --auto-add-overlay \
+    --extra-packages android.support.v7.appcompat \
+    --extra-packages android.support.v7.preference \
+    --extra-packages android.support.v7.recyclerview \
+    --extra-packages android.support.v14.preference \
+    --extra-packages com.citrus.settings
 
 ifneq ($(INCREMENTAL_BUILDS),)
     LOCAL_PROGUARD_ENABLED := disabled
