rohithbijoy21/packages_apps_Settings

Settings: Add volume tones back [2/2]
This patch allows the users to choose if they want the audible tone when adjusting the volume

Cardinal edits:
 * Adapt to new oreo settings

Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

src/com/android/settings/notification/VolumeAdjustPreferenceController.java
@@ -1,0 +1,37 @@
+/*
+ * Copyright (C) 2017 Cardinal-AOSP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.notification;
+
+import static com.android.settings.notification.SettingPref.TYPE_SYSTEM;
+
+import android.content.Context;
+
+import android.provider.Settings.System;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.core.lifecycle.Lifecycle;
+
+public class VolumeAdjustPreferenceController extends SettingPrefController {
+
+    private static final String KEY_VOLUME_ADJUST_SOUNDS = "volume_adjust_sounds";
+
+    public VolumeAdjustPreferenceController(Context context, SettingsPreferenceFragment parent,
+            Lifecycle lifecycle) {
+        super(context, parent, lifecycle);
+        mPreference = new SettingPref(
+            TYPE_SYSTEM, KEY_VOLUME_ADJUST_SOUNDS, System.VOLUME_ADJUST_SOUNDS_ENABLED, DEFAULT_ON);
+    }
+}

shivatejapeddi/packages_apps_Settings-1

Change-Id: I08cac55ca7b0df3ff87a4d9da4d51b2a2369647a

src/com/android/settings/notification/VolumeAdjustPreferenceController.java
@@ -1,0 +1,37 @@
+/*
+ * Copyright (C) 2017 Cardinal-AOSP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.notification;
+
+import static com.android.settings.notification.SettingPref.TYPE_SYSTEM;
+
+import android.content.Context;
+
+import android.provider.Settings.System;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settingslib.core.lifecycle.Lifecycle;
+
+public class VolumeAdjustPreferenceController extends SettingPrefController {
+
+    private static final String KEY_VOLUME_ADJUST_SOUNDS = "volume_adjust_sounds";
+
+    public VolumeAdjustPreferenceController(Context context, SettingsPreferenceFragment parent,
+            Lifecycle lifecycle) {
+        super(context, parent, lifecycle);
+        mPreference = new SettingPref(
+            TYPE_SYSTEM, KEY_VOLUME_ADJUST_SOUNDS, System.VOLUME_ADJUST_SOUNDS_ENABLED, DEFAULT_ON);
+    }
+}