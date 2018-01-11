rohithbijoy21/packages_apps_Settings

Settings: long press edit button to launch QS settings [2/2]
Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -128,6 +128,7 @@ import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 
 import com.cardinal.settings.WingsSettings;
+import com.cardinal.settings.fragments.QuickSettings;
 import com.cardinal.settings.fragments.NavigationBarSettings;
 
 public class SettingsGateway {
@@ -249,6 +250,7 @@ public class SettingsGateway {
             LockscreenDashboardFragment.class.getName(),
             WingsSettings.class.getName(),
             NavigationBarSettings.class.getName(),
+            QuickSettings.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {

shivatejapeddi/packages_apps_Settings-1

Change-Id: Ica0c334fa3b6b0a227768a5f64e78cbb9fc27a70

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -137,6 +137,7 @@ import com.citrus.settings.fragments.FlingSettings;
 import com.citrus.settings.fragments.NavigationBar;
 import com.citrus.settings.fragments.PulseSettings;
 import com.citrus.settings.fragments.SmartbarSettings;
+import com.citrus.settings.fragments.QuickSettings;
 
 public class SettingsGateway {
 
@@ -263,7 +264,8 @@ public class SettingsGateway {
             NavigationBar.class.getName(),
             FlingSettings.class.getName(),
             SmartbarSettings.class.getName(),
-            PulseSettings.class.getName()
+            PulseSettings.class.getName(),
+            QuickSettings.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {