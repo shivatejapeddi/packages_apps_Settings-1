mady51/packages_apps_Settings

Settings: Add activity for launching Rotation settings
Change-Id: I2f270a1c4f3b084153b4f67e44e3bdf9c6690611
Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -137,6 +137,7 @@ import com.cardinal.settings.fragments.NotificationStyleSettings;
 import com.cardinal.settings.fragments.QuickSettings;
 import com.cardinal.settings.fragments.NavigationBarSettings;
 import com.cardinal.settings.fragments.NotificationSettings;
+import com.android.settings.custom.DisplayRotation;
 
 public class SettingsGateway {
 
@@ -263,7 +264,8 @@ public class SettingsGateway {
             NavigationBarSettings.class.getName(),
             QuickSettings.class.getName(),
             NotificationStyleSettings.class.getName(),
-            NotificationSettings.class.getName()
+            NotificationSettings.class.getName(),
+            DisplayRotation.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {

shivatejapeddi/packages_apps_Settings-1

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -138,6 +138,7 @@ import com.citrus.settings.fragments.NavigationBar;
 import com.citrus.settings.fragments.PulseSettings;
 import com.citrus.settings.fragments.SmartbarSettings;
 import com.citrus.settings.fragments.QuickSettings;
+import com.android.settings.custom.DisplayRotation;
 
 public class SettingsGateway {
 
@@ -265,7 +266,8 @@ public class SettingsGateway {
             FlingSettings.class.getName(),
             SmartbarSettings.class.getName(),
             PulseSettings.class.getName(),
-            QuickSettings.class.getName()
+            QuickSettings.class.getName(),
+            DisplayRotation.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {