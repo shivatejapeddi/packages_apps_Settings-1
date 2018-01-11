DarkUnicornss/settings

DUI: Initial checkin for Oreo [3/7]
Change-Id: I5ea7016c221c0e85752180cd784ec56f715e2c02

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -129,6 +129,10 @@ import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 
 import com.dirtyunicorns.tweaks.DirtyTweaks;
+import com.dirtyunicorns.tweaks.fragments.FlingSettings;
+import com.dirtyunicorns.tweaks.fragments.NavigationBar;
+import com.dirtyunicorns.tweaks.fragments.PulseSettings;
+import com.dirtyunicorns.tweaks.fragments.SmartbarSettings;
 
 public class SettingsGateway {
 
@@ -249,6 +253,10 @@ public class SettingsGateway {
             WebViewAppPicker.class.getName(),
             LockscreenDashboardFragment.class.getName(),
             DirtyTweaks.class.getName(),
+            NavigationBar.class.getName(),
+            FlingSettings.class.getName(),
+            SmartbarSettings.class.getName(),
+            PulseSettings.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -288,5 +296,9 @@ public class SettingsGateway {
             Settings.DeviceInfoSettingsActivity.class.getName(),
             Settings.EnterprisePrivacySettingsActivity.class.getName(),
             Settings.DirtyTweaksActivity.class.getName(),
+            Settings.NavigationSettingsActivity.class.getName(),
+            Settings.FlingSettingsActivity.class.getName(),
+            Settings.SmartbarSettingsActivity.class.getName(),
+            Settings.PulseSettingsActivity.class.getName()
     };
 }

shivatejapeddi/packages_apps_Settings-1

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -133,6 +133,10 @@ import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 
 import com.citrus.settings.CustomSquash;
+import com.citrus.settings.fragments.FlingSettings;
+import com.citrus.settings.fragments.NavigationBar;
+import com.citrus.settings.fragments.PulseSettings;
+import com.citrus.settings.fragments.SmartbarSettings;
 
 public class SettingsGateway {
 
@@ -255,7 +259,11 @@ public class SettingsGateway {
             LockscreenDashboardFragment.class.getName(),
             BluetoothDeviceDetailsFragment.class.getName(),
             DataUsageList.class.getName(),
-            CustomSquash.class.getName()
+            CustomSquash.class.getName(),
+            NavigationBar.class.getName(),
+            FlingSettings.class.getName(),
+            SmartbarSettings.class.getName(),
+            PulseSettings.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -294,5 +302,9 @@ public class SettingsGateway {
             Settings.DeviceInfoSettingsActivity.class.getName(),
             Settings.EnterprisePrivacySettingsActivity.class.getName(),
             Settings.CustomSquashActivity.class.getName(),
+            Settings.NavigationSettingsActivity.class.getName(),
+            Settings.FlingSettingsActivity.class.getName(),
+            Settings.SmartbarSettingsActivity.class.getName(),
+            Settings.PulseSettingsActivity.class.getName()
     };
 }